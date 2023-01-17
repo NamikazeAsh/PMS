b2c711adc0dd87a8f816df939f523615ece043eb
Balakrishna R <105032444+balakrishna1202@users.noreply.github.com>
2023-01-17 10:41:43 +0530

    Removed password

--- consultancy2/settings.py
+++ consultancy2/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'finaldb1',
         'USER' : 'root',
-        'PASSWORD' : '1234',
+        'PASSWORD' : '',
         'HOST' : 'localhost',
         'PORT' : '3306',
         'OPTIONS' : {
