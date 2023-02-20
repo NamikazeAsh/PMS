a774a30f8ae977eb26d3682fb237001baec0d7e9
primundhraa <primundhraa@gmail.com>
2023-02-20 15:37:06 +0530

    making model for finance

--- finance/models.py
+++ finance/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Finance(models.Model):
+    
+    amt_received = models.FloatField(default=0)
+    cu_percentage =models.FloatField(default=0)
+    expenses = models.JSONField()
+    income = models.JSONField()
+    net_amt = models.FloatField(default=0)
+    professor = models.JSONField()
+
+    
