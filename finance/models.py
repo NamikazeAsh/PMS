8c60600770f97ce215616118a91193f660dd4b6d
Balakrishna R <105032444+balakrishna1202@users.noreply.github.com>
2023-02-21 10:34:41 +0530

    ..

--- finance/models.py
+++ finance/models.py
@@ -1,29 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
-<<<<<<< HEAD
-# from django.contrib.postgres.fields import JSONField
-from jsonfield import JSONField
-
-# Create your models here.
-class ProjectFinance(models.Model):
-   
-    project_id = models.ForeignKey("projects.Project", on_delete=models.CASCADE)
-=======
 
 # Create your models here.
 
 class Finance(models.Model):
     
->>>>>>> 28f9b53e2ce6269cd38951cf7aac1aa71ba62bd1
     amt_received = models.FloatField(default=0)
     cu_percentage =models.FloatField(default=0)
     expenses = models.JSONField()
     income = models.JSONField()
     net_amt = models.FloatField(default=0)
-<<<<<<< HEAD
-    professor = models.JSONField()
-=======
     professor = models.JSONField()
 
     
->>>>>>> 28f9b53e2ce6269cd38951cf7aac1aa71ba62bd1
