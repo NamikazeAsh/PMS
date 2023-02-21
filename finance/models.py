dc920fffade407b4eeebba28e50992dca4c7d4a8
primundhraa <primundhraa@gmail.com>
2023-02-21 10:35:58 +0530

    .....

--- finance/models.py
+++ finance/models.py
@@ -3,13 +3,17 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Finance(models.Model):
+class ProjectFinance(models.Model):
     
+    # project_id = models.ForeignKey("projects.Project", on_delete=models.CASCADE)
     amt_received = models.FloatField(default=0)
     cu_percentage =models.FloatField(default=0)
     expenses = models.JSONField()
     income = models.JSONField()
     net_amt = models.FloatField(default=0)
     professor = models.JSONField()
+
+    # def __str__(self):
+    #     return (self.project_id)
 
     
