cedc2dc1ae825799c63df67e21d978d8d72a9fe6
Balakrishna R <105032444+balakrishna1202@users.noreply.github.com>
2023-03-23 08:58:54 +0530

    ...

--- calendarapp/models/event_member.py
+++ calendarapp/models/event_member.py
@@ -8,12 +8,10 @@ class EventMember(EventAbstract):
     """ Event member model """
 
     event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name="events")
-    user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="event_members"
+    user = models.ManyToManyField(
+        User,related_name="event_members"
     )
 
-    class Meta:
-        unique_together = ["event", "user"]
 
     def __str__(self):
         return str(self.user)
