b34817f57eeb793ef74ca6b69af8ef548b659477
NamikazeAsh <ashwinsatish0@gmail.com>
2023-04-02 10:14:33 +0530

    Error page 1/2

--- consultancy2/decorators.py
+++ consultancy2/decorators.py
@@ -29,19 +29,3 @@ def allowed_users(allowed_roles=[]):
             return view_func(request,*args,**kwargs)
         return wrapper_func
     return decorator
-
-
-# def admin_only(view_func):
-#     def wrapper_function(request,*args,**kwargs):
-#         group = None
-#         if request.user.groups.exists():
-#             group = request.user.groups.all()[0].name
-            
-#         if group == 'Intern':
-#             return redirect('temp-intern')
-#         if group == 'Sr Intern':
-#             return redirect('temp-srintern')
-#         if group == 'admin':
-#             return view_func(request,*args,**kwargs)
-        
-#     return wrapper_function
