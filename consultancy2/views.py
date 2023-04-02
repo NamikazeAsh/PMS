9b0799958666fabfe9e5ad9fa74beb42ed113bcd
NamikazeAsh <ashwinsatish0@gmail.com>
2023-04-02 10:08:13 +0530

    Fixed template render error, milestone pg error

--- consultancy2/views.py
+++ consultancy2/views.py
@@ -36,9 +36,9 @@ def findtemp(request):
     elif request.user.groups.filter(name='Professor').exists():
         return 'srintern/tempsrintern.html'
     elif request.user.groups.filter(name='Lead Consultant').exists():
-        return 'consultant/tempprof.html'
+        return 'consultant/templeadc.html'
     elif request.user.groups.filter(name='Head Consultant').exists():
-        return 'consultant/tempprof.html'
+        return 'consultant/tempheadc.html'
 
 @login_required(login_url='login')
 def HomePage(request):
