b14067375741f24187c9259f082207da3ac3a6b7
NamikazeAsh <ashwinsatish0@gmail.com>
2023-04-02 18:54:34 +0530

    Added admin-changelist for consultancy2

--- consultancy2/admin.py
+++ consultancy2/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import AdminValidation
 
-admin.site.register(AdminValidation)
+class AdminValidationAdmin(admin.ModelAdmin):
+    list_display = ['username','firstname','lastname']
+    list_display = ['username','firstname','lastname','campus','role']
+    search_fields = ['username','firstname','lastname','campus','role']
+
+    class Meta:
+        model = AdminValidation
+
+admin.site.register(AdminValidation,AdminValidationAdmin)
